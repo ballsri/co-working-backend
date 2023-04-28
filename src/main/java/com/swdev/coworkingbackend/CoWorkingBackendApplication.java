6d379c4c7d87635e972d221601ea8b303e603338
Natthaphon Suphansri <Natthaphon.s2018@gmail.com>
2023-04-28 21:40:00 +0700

    add a ping function

--- src/main/java/com/swdev/coworkingbackend/CoWorkingBackendApplication.java
+++ src/main/java/com/swdev/coworkingbackend/CoWorkingBackendApplication.java
@@ -2,13 +2,20 @@ package com.swdev.coworkingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestCon
+@RestController
 public class CoWorkingBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoWorkingBackendApplication.class, args);
 	}
 
+	@GetMapping("/ping")
+	public String ping(){
+		return String.format("the server is up and responding to ping");
+	}
 }
