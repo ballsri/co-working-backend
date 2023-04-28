ad4fd176092d9e677d9fd0f262557148becc9046
Natthaphon Suphansri <Natthaphon.s2018@gmail.com>
2023-04-28 21:29:42 +0700

    initialize spring

---
+++ co-working-backend/src/main/java/com/swdev/coworkingbackend/CoWorkingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swdev.coworkingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoWorkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoWorkingBackendApplication.class, args);
+	}
+
+}
---
+++ co-working-backend/src/test/java/com/swdev/coworkingbackend/CoWorkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swdev.coworkingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoWorkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
